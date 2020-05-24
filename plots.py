7723a1f941c07298eb4263c6aa5a4470be159b36
Ethan Brooks <ethanabrooks@gmail.com>
2020-05-24 16:50:13 -0400

    got plots.py to work

--- plots.py
+++ plots.py
@@ -1,16 +1,30 @@
 #! /usr/bin/env python
 
-from rl_utils import plot
 from pathlib import Path
 import csv
-import subprocess
+from libtmux import Session, Server
 
 
 def main(plots_data: Path, runs_data: Path, limit: int):
     with plots_data.open() as plots_file, runs_data.open() as runs_file:
+        runs_reader = csv.DictReader(runs_file)
+        rows = [r for r in runs_reader]
         plots_reader = csv.reader(plots_file)
-        for tag, fname in plots_reader:
-            subprocess.run(["ls", "-l"])
+        next(plots_reader)  # skip header
+        names = [f"'{r['algorithm']}'" for r in rows]
+        paths = [f"'{r['local path']}'" for r in rows]
+        server = Server()
+        session = server.new_session(session_name="plots", kill_session=True)
+        for tag, path in plots_reader:
+            cmd = " ".join(
+                "plot --names".split()
+                + names
+                + ["--base-dir", "''"]
+                + ["--paths"]
+                + paths
+                + ["--tag", tag, "--limit", str(limit), "--fname", path]
+            )
+            window = session.new_window(window_name=tag, attach=True, window_shell=cmd)
 
 
 if __name__ == "__main__":
@@ -20,4 +34,4 @@ if __name__ == "__main__":
     parser.add_argument("--plots-data", type=Path, default=Path("plots.csv"))
     parser.add_argument("--runs-data", type=Path, default=Path("runs.csv"))
     parser.add_argument("--limit", type=int, required=True)
-    main(**vars(parser.arg_parse()))
+    main(**vars(parser.parse_args()))
--- rewards40-50/plot.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env zsh
-plot --names Ours 'No roll' 'No scan' 'No roll or scan' 'OLSK' Transformer --paths ~/neurips/ours ~/neurips/no-roll ~/neurips/no-scan ~/neurips/no-roll-or-scan ~/neurips/olsk ~/neurips/transformer --tag eval_success_40 --limit 80000000 --fname ~/neurips/rewards-40-50
