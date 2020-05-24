979c7f4ea16dddb213bcd92554856a0444f67cf9
Ethan Brooks <ethanabrooks@gmail.com>
2020-05-24 17:21:06 -0400

    add copying to plots.py

--- plots.py
+++ plots.py
@@ -3,28 +3,50 @@
 from pathlib import Path
 import csv
 from libtmux import Session, Server
+import subprocess
 
 
-def main(plots_data: Path, runs_data: Path, limit: int):
+def path_to_str(p):
+    return str(Path(p).expanduser())
+
+
+def main(plots_data: Path, runs_data: Path, limit: int, perform_copy: bool):
     with plots_data.open() as plots_file, runs_data.open() as runs_file:
-        runs_reader = csv.DictReader(runs_file)
-        rows = [r for r in runs_reader]
-        plots_reader = csv.reader(plots_file)
+        names = []
+        paths = []
+
+        def copy(origin_path, local_path, **_):
+            subprocess.run(
+                ["rsync", "-av", path_to_str(origin_path), path_to_str(local_path)]
+            )
+
+        def append(algorithm, local_path, **_):
+            names.append(f"'{algorithm}'")
+            paths.append(f"'{local_path}'")
+
+        for row in csv.DictReader(runs_file):
+            if perform_copy:
+                copy(**row)
+            append(**row)
+
+        plots_reader = csv.DictReader(plots_file)
         next(plots_reader)  # skip header
-        names = [f"'{r['algorithm']}'" for r in rows]
-        paths = [f"'{r['local path']}'" for r in rows]
-        server = Server()
-        session = server.new_session(session_name="plots", kill_session=True)
-        for tag, path in plots_reader:
+        session = Server().new_session(session_name="plots", kill_session=True)
+
+        def new_window(tag, path):
             cmd = " ".join(
-                "plot --names".split()
-                + names
+                ["plot"]
+                + ["--names", *names]
                 + ["--base-dir", "''"]
-                + ["--paths"]
-                + paths
-                + ["--tag", tag, "--limit", str(limit), "--fname", path]
+                + ["--paths", *paths]
+                + ["--tag", tag]
+                + ["--limit", str(limit)]
+                + ["--fname", path_to_str(path)]
             )
             window = session.new_window(window_name=tag, attach=True, window_shell=cmd)
+
+        for row in plots_reader:
+            new_window(**row)
 
 
 if __name__ == "__main__":
@@ -34,4 +56,5 @@ if __name__ == "__main__":
     parser.add_argument("--plots-data", type=Path, default=Path("plots.csv"))
     parser.add_argument("--runs-data", type=Path, default=Path("runs.csv"))
     parser.add_argument("--limit", type=int, required=True)
+    parser.add_argument("--no-copy", dest="perform_copy", action="store_false")
     main(**vars(parser.parse_args()))
