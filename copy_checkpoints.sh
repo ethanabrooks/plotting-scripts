ba9e82e3b53ebf64e29c055c184d14d620a71d17
Ethan Brooks <ethanabrooks@gmail.com>
2020-05-25 17:59:00 -0400

    gitignore

---
+++ copy_checkpoints.sh
@@ -0,0 +1,77 @@
+#! /usr/bin/env bash
+#mkdir -p checkpoints/no-roll-or-scan/0
+#mkdir -p checkpoints/no-roll-or-scan/1
+#mkdir -p checkpoints/no-roll-or-scan/2
+#mkdir -p checkpoints/no-roll-or-scan/3
+#mkdir -p checkpoints/no-roll-or-scan/4
+#scp rldl17:/home/ethanbro/ppo/.runs/logdir/neurips/no-roll-or-scan/0/checkpoint.pt checkpoints/no-roll-or-scan/0/checkpoint.pt
+#scp rldl17:/home/ethanbro/ppo/.runs/logdir/neurips/no-roll-or-scan/1/checkpoint.pt checkpoints/no-roll-or-scan/1/checkpoint.pt
+#scp rldl17:/home/ethanbro/ppo/.runs/logdir/neurips/no-roll-or-scan/2/checkpoint.pt checkpoints/no-roll-or-scan/2/checkpoint.pt
+#scp rldl17:/home/ethanbro/ppo/.runs/logdir/neurips/no-roll-or-scan/3/checkpoint.pt checkpoints/no-roll-or-scan/3/checkpoint.pt
+#scp rldl17:/home/ethanbro/ppo/.runs/logdir/neurips/no-roll-or-scan/4/checkpoint.pt checkpoints/no-roll-or-scan/4/checkpoint.pt
+
+#mkdir -p checkpoints/no-scan/0
+#mkdir -p checkpoints/no-scan/1
+#mkdir -p checkpoints/no-scan/2
+#mkdir -p checkpoints/no-scan/3
+#mkdir -p checkpoints/no-scan/4
+#scp rldl17:/home/ethanbro/ppo/.runs/logdir/neurips/no-scan/0/checkpoint.pt checkpoints/no-scan/0/checkpoint.pt
+#scp rldl17:/home/ethanbro/ppo/.runs/logdir/neurips/no-scan/1/checkpoint.pt checkpoints/no-scan/1/checkpoint.pt
+#scp rldl17:/home/ethanbro/ppo/.runs/logdir/neurips/no-scan/2/checkpoint.pt checkpoints/no-scan/2/checkpoint.pt
+#scp rldl17:/home/ethanbro/ppo/.runs/logdir/neurips/no-scan/3/checkpoint.pt checkpoints/no-scan/3/checkpoint.pt
+#scp rldl17:/home/ethanbro/ppo/.runs/logdir/neurips/no-scan/4/checkpoint.pt checkpoints/no-scan/4/checkpoint.pt
+
+#mkdir -p checkpoints/no-roll/0
+#mkdir -p checkpoints/no-roll/1
+#mkdir -p checkpoints/no-roll/2
+#mkdir -p checkpoints/no-roll/3
+#mkdir -p checkpoints/no-roll/4
+#scp rldl17:/home/ethanbro/ppo/.runs/logdir/neurips/no-roll/0/checkpoint.pt checkpoints/no-roll/0/checkpoint.pt
+#scp rldl17:/home/ethanbro/ppo/.runs/logdir/neurips/no-roll/1/checkpoint.pt checkpoints/no-roll/1/checkpoint.pt
+#scp rldl17:/home/ethanbro/ppo/.runs/logdir/neurips/no-roll/2/checkpoint.pt checkpoints/no-roll/2/checkpoint.pt
+#scp rldl17:/home/ethanbro/ppo/.runs/logdir/neurips/no-roll/3/checkpoint.pt checkpoints/no-roll/3/checkpoint.pt
+#scp rldl17:/home/ethanbro/ppo/.runs/logdir/neurips/no-roll/4/checkpoint.pt checkpoints/no-roll/4/checkpoint.pt
+
+#mkdir -p checkpoints/ours/0
+#mkdir -p checkpoints/ours/1
+#mkdir -p checkpoints/ours/2
+#mkdir -p checkpoints/ours/3
+#mkdir -p checkpoints/ours/4
+#scp rldl16:/home/ethanbro/ppo/.runs/logdir/neurips/ours2/0/checkpoint.pt checkpoints/ours/0/checkpoint.pt
+#scp rldl16:/home/ethanbro/ppo/.runs/logdir/neurips/ours2/1/checkpoint.pt checkpoints/ours/1/checkpoint.pt
+#scp rldl16:/home/ethanbro/ppo/.runs/logdir/neurips/ours2/2/checkpoint.pt checkpoints/ours/2/checkpoint.pt
+#scp rldl16:/home/ethanbro/ppo/.runs/logdir/neurips/ours2/3/checkpoint.pt checkpoints/ours/3/checkpoint.pt
+#scp rldl16:/home/ethanbro/ppo/.runs/logdir/neurips/ours2/4/checkpoint.pt checkpoints/ours/4/checkpoint.pt
+
+#mkdir -p checkpoints/transformer/0
+#mkdir -p checkpoints/transformer/1
+#mkdir -p checkpoints/transformer/2
+#mkdir -p checkpoints/transformer/3
+#mkdir -p checkpoints/transformer/4
+#scp rldl15:/home/ethanbro/ppo/.runs/logdir/neurips/transformer/0/checkpoint.pt checkpoints/transformer/0/checkpoint.pt
+#scp rldl15:/home/ethanbro/ppo/.runs/logdir/neurips/transformer/1/checkpoint.pt checkpoints/transformer/1/checkpoint.pt
+#scp rldl15:/home/ethanbro/ppo/.runs/logdir/neurips/transformer/2/checkpoint.pt checkpoints/transformer/2/checkpoint.pt
+#scp rldl15:/home/ethanbro/ppo/.runs/logdir/neurips/transformer/3/checkpoint.pt checkpoints/transformer/3/checkpoint.pt
+#scp rldl15:/home/ethanbro/ppo/.runs/logdir/neurips/transformer/4/checkpoint.pt checkpoints/transformer/4/checkpoint.pt
+
+mkdir -p checkpoints/olsk/0
+mkdir -p checkpoints/olsk/1
+mkdir -p checkpoints/olsk/2
+mkdir -p checkpoints/olsk/3
+mkdir -p checkpoints/olsk/4
+scp rldl16:/home/ethanbro/ppo/.runs/logdir/neurips/olsk/0/checkpoint.pt checkpoints/olsk/0/checkpoint.pt
+scp rldl16:/home/ethanbro/ppo/.runs/logdir/neurips/olsk/1/checkpoint.pt checkpoints/olsk/1/checkpoint.pt
+scp rldl16:/home/ethanbro/ppo/.runs/logdir/neurips/olsk/2/checkpoint.pt checkpoints/olsk/2/checkpoint.pt
+scp rldl16:/home/ethanbro/ppo/.runs/logdir/neurips/olsk/3/checkpoint.pt checkpoints/olsk/3/checkpoint.pt
+scp rldl16:/home/ethanbro/ppo/.runs/logdir/neurips/olsk/4/checkpoint.pt checkpoints/olsk/4/checkpoint.pt
+
+mkdir -p checkpoints/no-pointer/0
+mkdir -p checkpoints/no-pointer/1
+mkdir -p checkpoints/no-pointer/2
+mkdir -p checkpoints/no-pointer/3
+mkdir -p checkpoints/no-pointer/4
+scp rldl14:/home/ethanbro/ppo/.runs/logdir/neurips/no-pointer/0/checkpoint.pt checkpoints/no-pointer/0/checkpoint.pt
+scp rldl14:/home/ethanbro/ppo/.runs/logdir/neurips/no-pointer/1/checkpoint.pt checkpoints/no-pointer/1/checkpoint.pt
+scp rldl14:/home/ethanbro/ppo/.runs/logdir/neurips/no-pointer/2/checkpoint.pt checkpoints/no-pointer/2/checkpoint.pt
+scp rldl14:/home/ethanbro/ppo/.runs/logdir/neurips/no-pointer/3/checkpoint.pt checkpoints/no-pointer/3/checkpoint.pt
+scp rldl14:/home/ethanbro/ppo/.runs/logdir/neurips/no-pointer/4/checkpoint.pt checkpoints/no-pointer/4/checkpoint.pt
